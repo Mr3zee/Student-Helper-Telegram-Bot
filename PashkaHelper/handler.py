b1acfb11ba6f93c29f0df258199cc2b67e64996d
Mr3zee <284627@niuitmo.ru>
2020-09-10 14:09:19 +0300

    Pashka Bot: subject handlers added

--- PashkaHelper/handler.py
+++ PashkaHelper/handler.py
@@ -1,4 +1,4 @@
-from telegram.ext import MessageHandler, CallbackContext, Filters
+from telegram.ext import MessageHandler, CommandHandler, CallbackContext, Filters
 from telegram import Update
 
 from PashkaHelper.log import log_handler
@@ -9,12 +9,84 @@ handlers = {}
 
 
 @log_handler
-def echo(update: Update, context: CallbackContext):
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['start_text'],
+    )
+
+
+@log_handler
+def help_(update: Update, context: CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=text['ru']['help_text'],
     )
-    return 0
 
 
-handlers['echo'] = MessageHandler(filters=Filters.all, callback=echo)
+@log_handler
+def algo(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['algo_text'],
+    )
+
+
+@log_handler
+def matan(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['matan_text'],
+    )
+
+
+@log_handler
+def kotlin(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['kotlin_text'],
+    )
+
+
+@log_handler
+def os(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['os_text'],
+    )
+
+
+@log_handler
+def diffur(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['diffur_text'],
+    )
+
+
+@log_handler
+def discra(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['discra_text'],
+    )
+
+
+@log_handler
+def eng(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text['ru']['eng_text'],
+    )
+
+
+handlers['start'] = CommandHandler(command='start', callback=start)
+handlers['help'] = CommandHandler(command='help', callback=help_)
+handlers['algo'] = CommandHandler(command='algo', callback=algo)
+handlers['discra'] = CommandHandler(command='discra', callback=discra)
+handlers['diffur'] = CommandHandler(command='diffur', callback=diffur)
+handlers['os'] = CommandHandler(command='os', callback=os)
+handlers['kotlin'] = CommandHandler(command='kotlin', callback=kotlin)
+handlers['matan'] = CommandHandler(command='matan', callback=matan)
+handlers['eng'] = CommandHandler(command='eng', callback=eng)
+
