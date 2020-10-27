5e997fefb1ba114d062492f9bc915776050c36b2
Mr3zee <284627@niuitmo.ru>
2020-10-28 01:03:32 +0300

    Pashka Bot: timetable improvements

--- PashkaHelper/src/timetable.py
+++ PashkaHelper/src/timetable.py
@@ -2,28 +2,29 @@ from src.text import get_text
 from static import consts
 from src.server import Server
 
-subject_template = '%(time)s | %(subject)s | %(teacher)s | %(place)s'
-subject_template_parity = '%(time)s | %(parity)s | %(subject)s | %(teacher)s | %(place)s'
+SUBJECT_TEMPLATE = '%(time)s | %(subject)s | %(teacher)s | %(place)s'
+SUBJECT_TEMPLATE_WITH_PARITY = '%(time)s | %(parity)s | %(subject)s | %(teacher)s | %(place)s'
 
 SERVER = Server.get_instance()
 
 
 def get_subject_timetable(subject, subtype, attendance, language_code):
     """get a timetable for specified subject"""
+
+    # dict of {weekday: {'online': tt1, 'offline': tt2}}
     timetable = SERVER.get_subject_timetable(subject, subtype, attendance)
 
     # no timetable for this subject
     if not timetable:
         return get_text('no_subject_timetable_header_text', language_code=language_code).text()
 
-    template = get_text('subject_timetable_header_text', language_code=language_code).text()
     weekday_list = []
-    # timetable is dict of weekday: ([online, None] or [offline, None]) xor [online, offline]
-    for weekday, (sub1, sub2) in timetable.items():
-        # get header
+    for weekday, dct in timetable.items():
+        # get subheader
         weekday_name = get_text(f'{weekday}_timetable_text', language_code=language_code).text()
-        # get timetable
-        subject_timetable = __put_together(sub1, sub2, attendance, subject_template_parity, language_code)
+
+        # make timetable
+        subject_timetable = __put_together(template=SUBJECT_TEMPLATE_WITH_PARITY, language_code=language_code, **dct)
         if not subject_timetable:
             continue
         day_template = get_text('subject_day_template_text', language_code=language_code).text({
@@ -32,7 +33,9 @@ def get_subject_timetable(subject, subtype, attendance, language_code):
         })
         weekday_list.append(day_template)
 
-    return template + '\n'.join(weekday_list)
+    header = get_text('subject_timetable_header_text', language_code=language_code).text()
+
+    return header + '\n'.join(weekday_list)
 
 
 def get_weekday_timetable(weekday: str, subject_names, attendance, week_parity, language_code) -> str:
@@ -47,65 +50,51 @@ def get_weekday_timetable(weekday: str, subject_names, attendance, week_parity, 
 
     # get text templates
     template = get_text('weekday_text', language_code)
-    weekday_text = get_text(f'{weekday}_timetable_text', language_code).text()
 
-    # get timetables as lists
-    # ((online, None) or (offline, None)) xor (online, offline)
-    first_subject_list, second_subject_list = SERVER.get_weekday_timetable(
+    template.add_global_vars({
+        consts.WEEKDAY: get_text(f'{weekday}_timetable_text', language_code).text(),
+        consts.WEEK_PARITY: get_text(f'{week_parity}_week_timetable_text', language_code=language_code).text(),
+    })
+
+    # get timetables as dict
+    subject_tt: dict = SERVER.get_weekday_timetable(
         weekday=weekday,
         valid_subject_names=subject_names,
         attendance=attendance,
         week_parity=week_parity
     )
-    parity_text = get_text(f'{week_parity}_week_timetable_text', language_code=language_code).text()
 
-    template.add_global_vars({
-        consts.WEEKDAY: weekday_text,
-        consts.WEEK_PARITY: parity_text,
-    })
+    online_tt, offline_tt = subject_tt['online'], subject_tt['offline']
 
     # no subjects that day
-    if not first_subject_list and not second_subject_list:
+    if not online_tt and not offline_tt:
         happy_text = get_text('happy_timetable_text', language_code=language_code).text()
         return template.text({consts.TIMETABLE: happy_text})
 
-    # make timetable
+    # make full timetable
     weekday_timetable = __put_together(
-        first_subject_list=first_subject_list,
-        second_subject_list=second_subject_list,
-        attendance=attendance,
-        template=subject_template,
+        online=online_tt,
+        offline=offline_tt,
+        template=SUBJECT_TEMPLATE,
         language_code=language_code,
     )
     return template.text({consts.TIMETABLE: weekday_timetable})
 
 
-def __put_together(first_subject_list, second_subject_list, attendance, template, language_code):
+def __put_together(online, offline, template, language_code):
     """make header(s) and glue it to timetable(s)"""
-    first = second = ''
+    timetable = []
 
-    # if attendance is both then first is online
-    if attendance == consts.ATTENDANCE_BOTH:
-        attendance = consts.ATTENDANCE_ONLINE
-
-    if first_subject_list:
-        first = get_text(f'{attendance}_timetable_text', language_code).text() + '\n'
-        first += __make_timetable(first_subject_list, template)
+    if online:
+        header = get_text(f'online_timetable_text', language_code).text() + '\n'
+        timetable.append(header + __make_timetable(online, template))
 
     # only True when attendance is both and in that case first is online and second is offline
-    if second_subject_list:
-        second = get_text('offline_timetable_text', language_code).text() + '\n'
-        second += __make_timetable(second_subject_list, template)
+    if offline:
+        header = get_text('offline_timetable_text', language_code).text() + '\n'
+        timetable.append(header + __make_timetable(offline, template))
 
-    timetable = ''
-
-    if first != '':
-        timetable += first
-
-    if second != '':
-        timetable += ('\n\n' if first else '') + second
-
-    return timetable
+    return '\n\n'.join(timetable)
 
 
 def __rm_blanks(subject_row):
