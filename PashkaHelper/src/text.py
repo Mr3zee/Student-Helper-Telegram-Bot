3097dfc070d336556d3f2fed9320ec952b9da82a
Mr3zee <284627@niuitmo.ru>
2020-10-28 17:51:57 +0300

    Pashka Bot: links in other file now

--- PashkaHelper/src/text.py
+++ PashkaHelper/src/text.py
@@ -1,27 +1,31 @@
 from json import load
-from static.config import text_json_file_path, text_mlw_file_path
+from static.config import text_json_file_path, text_mlw_file_path, links_json_file_path
 from util.mlw_tools import MLWText, mlw_load
 
 # open .json file with texts
 with open(text_json_file_path, 'r', encoding='UTF-8') as json_file:
-    # open .mlw file with texts
-    with open(text_mlw_file_path, 'r', encoding='UTF-8') as mlw_file:
-        # load mlw contents
-        mlw_text = mlw_load(mlw_file)
-        # load json contents
-        json_text = load(json_file)
+    json_text = load(json_file)
 
-        # currently only russian language is supported
-        data = {'ru': {}}
+# open .mlw file with texts
+with open(text_mlw_file_path, 'r', encoding='UTF-8') as mlw_file:
+    mlw_text = mlw_load(mlw_file)
 
-        # convert json to mlw and push into dict
-        for language, text in json_text.items():
-            for key, value in text.items():
-                data[language][key] = MLWText(key, [value])
+# open links.json file
+with open(links_json_file_path, 'r', encoding='UTF-8') as links_file:
+    links_dict = load(links_file)
 
-        # push mlw into dict
-        for key, value in mlw_text.items():
-            data['ru'][key] = value
+# currently only russian language is supported
+data = {'ru': {}}
+
+# convert json to mlw and push into dict
+for language, text in json_text.items():
+    for key, value in text.items():
+        data[language][key] = MLWText(key, [value])
+
+# push mlw into dict
+for key, value in mlw_text.items():
+    value.add_local_dict(links_dict, ['32', '36-37'])
+    data['ru'][key] = value
 
 
 def get_text(name, language_code) -> MLWText:
